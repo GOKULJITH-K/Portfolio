b71681f9d54e92a4a2fcdadbe767b7acf1039d3f
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-06-23 17:00:39 +0200

    Add files via upload

--- Portfolio/src/components/Webdev/Webdev.jsx
+++ Portfolio/src/components/Webdev/Webdev.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import profile_pic from '../../assets/profile pic.jpg'
+import profile_pic from '../../assets/telinso.png'
+import spotify from '../../assets/spotify.png'
+import portfolio from '../../assets/portfolio.png'
 import './webdev.css'
  const Webdev = () => {
+    const handleCardClick = (page) => {
+        window.location.href = page ;
+    }
   return (
     <div className='web'>
         <h1>Website Development</h1>
@@ -9,68 +14,35 @@ import './webdev.css'
             <div className="web-card">
             <h4>01 Soil monitoring app</h4>
                 <img src={profile_pic} alt='' />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor  </p>
+                <p>Telinso is a user-friendly, cost-effective, and scalable system that can help farmers optimize their farming practices and achieve sustainable agriculture.</p>
                 <div className="links">
                     <a href='' >Know more</a>
-                    <a href=''>Live view</a>
+                    <a onClick={()=> handleCardClick('https:telinso.onrender.com')} style={{ cursor: 'pointer' }}>Live view</a>
                 </div>
                 
             </div>
             <div className="web-card">
-            <h4>01 Soil monitoring app</h4>
-                <img src={profile_pic} alt='' />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor  </p>
+            <h4>02 Spotify</h4>
+                <img src={spotify} alt='' />
+                <p>Built a user-friendly Spotify clone for seamless streaming of your favorite music tracks.</p>
                 <div className="links">
                     <a href='' >Know more</a>
-                    <a href=''>Live view</a>
+                    <a onClick={()=> handleCardClick('https://spotify-j5me.onrender.com/')} style={{ cursor: 'pointer' }}>Live view</a>
                 </div>
                 
             </div>
             <div className="web-card">
-                <h4>01 Soil monitoring app</h4>
-                <img src={profile_pic} alt='' />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor  </p>
+                <h4>03 Portfolio</h4>
+                <img src={portfolio} alt='' />
+                <p>Explore my portfolio showcasing creative projects and skills in web design and development.</p>
                 <div className="links">
                     <a href='' >Know more</a>
-                    <a href=''>Live view</a>
+                    <a onClick={()=> handleCardClick('https://portfolio-zqy0.onrender.com/')} style={{ cursor: 'pointer' }}>Live view</a>
                 </div>
                 
             </div>
         </div>
-        <div className="web-list">
-            <div className="web-card">
-            <h4>01 Soil monitoring app</h4>
-                <img src={profile_pic} alt='' />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor  </p>
-                <div className="links">
-                    <a href='' >Know more</a>
-                    <a href=''>Live view</a>
-                </div>
-                
-            </div>
-            <div className="web-card">
-            <h4>01 Soil monitoring app</h4>
-                <img src={profile_pic} alt='' />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor  </p>
-                <div className="links">
-                    <a href='' >Know more</a>
-                    <a href=''>Live view</a>
-                </div>
- 
-            </div>
-            <div className="web-card">
-            <h4>01 Soil monitoring app</h4>
-                <img src={profile_pic} alt='' />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor  </p>
-                <div className="links">
-                    <a href='' >Know more</a>
-                    <a href=''>Live view</a>
-                </div>
-                
-            </div>
-           
-        </div>
-       
+        
     </div>
   )
 }
